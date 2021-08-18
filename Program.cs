dacf736197c4666c2bd7f747b31c90a778a8bb68
Hasanov Asadullo Abduvosievich <astromobile95@mail.ru>
2021-08-18 07:03:43 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person i = new Person();
+            i.name = "Dana";
+            i.age = 35;
+            i.weight = 75;
+            i.growth = 178;
+            i.eyes = "blue";        
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            
+            Console.WriteLine($"Name:{i.name}, Age:{i.age}, Wieght:{i.weight}, Eyes: {i.eyes}");
+
+        }
+        class Person 
+        {
+            public int age;
+            public string name;
+            public string eyes;
+            public int weight;
+            public int growth;
+            private string status;
+            
+            
+        }
+    }
+}
